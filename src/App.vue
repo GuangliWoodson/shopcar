73c08806c7e8f1b98d0d6250b6408465b18482d4
GuangliWoodson <guangliwoodson@outlook.com>
2022-07-19 17:00:56 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader :title="'我的购物车'" backgroundColor="red" color="green"></MyHeader>
+    <div style="margin-top: 45px">
+      <MyGoods v-for="item in list" :key="item.id" :item="item"></MyGoods>
+    </div>
+    <MyFooter :arr="list"></MyFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader'
+import MyGoods from './components/MyGoods'
+import MyFooter from './components/MyFooter'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MyHeader,
+    MyGoods,
+    MyFooter
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.$axios({
+      url: "/api/cart"
+    }).then((res) => {
+      console.log(res);
+      this.list = res.data.list
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyCount.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="my-counter">
+    <button type="button" class="btn btn-light" :disabled="itemGoods.goods_count == 0"
+      @click="itemGoods.goods_count > 1 && itemGoods.goods_count--">-</button>
+    <input type="number" class="form-control inp" v-model.number="itemGoods.goods_count">
+    <button type="button" class="btn btn-light" @click="itemGoods.goods_count++">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemGoods: {
+      type: Object,
+      default: () => ({}),
+    }
+  },
+  watch: {
+    itemGoods: {
+      deep: true,
+      handler(val) {
+        if (val.goods_count <= 0) {
+          val.goods_count = 1
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.my-counter {
+  display: flex;
+
+  .inp {
+    width: 45px;
+    text-align: center;
+    margin: 0 10px;
+  }
+
+  .btn,
+  .inp {
+    transform: scale(0.9);
+  }
+}
+</style>
